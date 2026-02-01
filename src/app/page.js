fa480d2fbebb1fa6eed71a7314bd0ae9587ebd13
“[Sandesh <[sandeshjadhao93094@gmail.com]>
2026-02-01 18:12:34 +0530

    fix: redirect root to login

--- src/app/page.js
+++ src/app/page.js
@@ -1,22 +1,5 @@
-import Header from "@/components/Header";
-import HabitCard from "@/components/HabitCard";
-import WeekGrid from "@/components/WeekGrid";
-import ProgressBar from "@/components/ProgressBar";
-import { habits } from "./data/dummyHabits";
+import { redirect } from "next/navigation";
 
 export default function Home() {
-  return (
-    <main className="max-w-md mx-auto p-4 space-y-6">
-      <Header />
-
-      <div className="space-y-3">
-        {habits.map(h => (
-          <HabitCard key={h.id} habit={h} />
-        ))}
-      </div>
-
-      <WeekGrid days={[1,1,0,1,1,1,0]} />
-      <ProgressBar value={86} />
-    </main>
-  );
+  redirect("/login");
 }
